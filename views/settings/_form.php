528772357f77fb8b632c2e9108fa169e52e7a281
hasan kiwan <developer@action.jo>
2025-03-12 21:32:58 +0300

    format

--- views/settings/_form.php
+++ views/settings/_form.php
@@ -11,32 +11,44 @@ use yii\widgets\ActiveForm;
 <div class="settings-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'facebook')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'instagram')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'snapchat')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'tiktok')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'youtube')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'fax')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'lag')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'lat')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-
+    <div class="row">
+        <div class="col-6">
+         <?= $form->field($model, 'facebook')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'instagram')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'snapchat')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'tiktok')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'youtube')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'fax')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'lag')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'lat')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-6">
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
