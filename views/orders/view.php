3f13d8fa6c4c283ddccb60786b75f2e9f5226c77
hasan kiwan <developer@action.jo>
2025-04-28 21:06:24 +0300

    Refactor order view: Streamline order items and summary display with Vue.js for improved user experience and maintainability.

--- views/orders/view.php
+++ views/orders/view.php
@@ -26,279 +26,115 @@ $orderId = $model->id;
 <div class="orders-view" id="order-app">
     <h1>{{ title }}</h1>
 
-    <div class="row d-flex justify-content-center align-items-center">
-            <div class="col-12">
-                <div class="card card-registration card-registration-2" style="border-radius: 15px;">
-                <div class="card-body p-0">
-                    <div class="row g-0">
-                    <div class="col-lg-8">
-                        <div class="p-5">
-                        <div class="d-flex justify-content-between align-items-center mb-5">
-                            <h1 class="fw-bold mb-0">{{ title }}</h1>
-                            <h6 class="mb-0 text-muted">{{ order.orderItems?.length}} items</h6>
-                        </div>
+    <!-- Order Status -->
+    <div class="order-status mb-4">
+        <span class="badge bg-primary">Order Status: {{ order.status?.name || 'N/A' }}</span>
+        <span class="badge bg-secondary ms-2">Payment Status: {{ order.payment_status || 'N/A' }}</span>
+        <span class="badge bg-success ms-2">Delivery Status: {{ order.delivery_status || 'N/A' }}</span>
+    </div>
 
+    <div class="row">
+        <div class="col-8">
+            <!-- Order Items -->
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h3 class="card-title">Order Items ({{ order.orderItems?.length || 0 }})</h3>
+                </div>
+                <div class="card-body">
+                    <div v-if="order.orderItems?.length">
+                        <div v-for="item in order.orderItems" :key="item.id" class="order-item mb-3 pb-3 border-bottom">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img v-if="item.product?.image_path" :src="'/'+ item.product.image_path" class="img-fluid" style="max-height: 100px;">
+                                    <div v-else class="bg-light text-center p-4">No Image</div>
+                                </div>
+                                <div class="col-md-6">
+                                    <h5>{{ item.product?.name || 'Product not found' }}</h5>
+                                    <div v-if="item.variant">
+                                        <small class="text-muted">Variant: {{ item.variant.name }}</small>
+                                    </div>
 
-                        <hr class="my-4">
-
-                        <div class="row mb-4 d-flex justify-content-between align-items-center">
-                            <div class="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img5.webp"
-                                class="img-fluid rounded-3" alt="Cotton T-shirt">
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="input-group">
+                                        <span class="input-group-text">Qty</span>
+                                        <input type="number" class="form-control" v-model="item.quantity" min="1" disabled>
+                                    </div>
+                                </div>
+                                <div class="col-md-2 text-end">
+                                    <h5>${{ (item.price * item.quantity).toFixed(2) }}</h5>
+                                    <small class="text-muted">${{ item.price }} each</small>
+                                </div>
                             </div>
-                            <div class="col-md-3 col-lg-3 col-xl-3">
-                            <h6 class="text-muted">Shirt</h6>
-                            <h6 class="mb-0">Cotton T-shirt</h6>
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                <i class="fas fa-minus"></i>
-                            </button>
-
-                            <input id="form1" min="0" name="quantity" value="1" type="number"
-                                class="form-control form-control-sm" />
-
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                <i class="fas fa-plus"></i>
-                            </button>
-                            </div>
-                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 class="mb-0">€ 44.00</h6>
-                            </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                            </div>
-                        </div>
-
-                        <hr class="my-4">
-
-                        <div class="row mb-4 d-flex justify-content-between align-items-center">
-                            <div class="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp"
-                                class="img-fluid rounded-3" alt="Cotton T-shirt">
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-3">
-                            <h6 class="text-muted">Shirt</h6>
-                            <h6 class="mb-0">Cotton T-shirt</h6>
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                <i class="fas fa-minus"></i>
-                            </button>
-
-                            <input id="form1" min="0" name="quantity" value="1" type="number"
-                                class="form-control form-control-sm" />
-
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                <i class="fas fa-plus"></i>
-                            </button>
-                            </div>
-                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 class="mb-0">€ 44.00</h6>
-                            </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                            </div>
-                        </div>
-
-                        <hr class="my-4">
-
-                        <div class="row mb-4 d-flex justify-content-between align-items-center">
-                            <div class="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img7.webp"
-                                class="img-fluid rounded-3" alt="Cotton T-shirt">
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-3">
-                            <h6 class="text-muted">Shirt</h6>
-                            <h6 class="mb-0">Cotton T-shirt</h6>
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                <i class="fas fa-minus"></i>
-                            </button>
-
-                            <input id="form1" min="0" name="quantity" value="1" type="number"
-                                class="form-control form-control-sm" />
-
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                <i class="fas fa-plus"></i>
-                            </button>
-                            </div>
-                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 class="mb-0">€ 44.00</h6>
-                            </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                            </div>
-                        </div>
-
-                        <hr class="my-4">
-
-                        <div class="pt-5">
-                            <h6 class="mb-0"><a href="#!" class="text-body"><i
-                                class="fas fa-long-arrow-alt-left me-2"></i>Back to shop</a></h6>
-                        </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 bg-body-tertiary">
-                        <div class="p-5">
-                        <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
-                        <hr class="my-4">
-
-                        <div class="d-flex justify-content-between mb-4">
-                            <h5 class="text-uppercase">items 3</h5>
-                            <h5>€ 132.00</h5>
-                        </div>
-
-                        <h5 class="text-uppercase mb-3">Shipping</h5>
-
-                        <div class="mb-4 pb-2">
-                            <select data-mdb-select-init>
-                            <option value="1">Standard-Delivery- €5.00</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                            <option value="4">Four</option>
-                            </select>
-                        </div>
-
-                        <h5 class="text-uppercase mb-3">Give code</h5>
-
-                        <div class="mb-5">
-                            <div data-mdb-input-init class="form-outline">
-                            <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Examplea2">Enter your code</label>
-                            </div>
-                        </div>
-
-                        <hr class="my-4">
-
-                        <div class="d-flex justify-content-between mb-5">
-                            <h5 class="text-uppercase">Total price</h5>
-                            <h5>€ 137.00</h5>
-                        </div>
-
-                        <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-block btn-lg"
-                            data-mdb-ripple-color="dark">Register</button>
-
-                        </div>
-                    </div>
+                    <div v-else class="text-center py-4">
+                        <p class="text-muted">No items found in this order</p>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="col-4">
+
+            <!-- Shipping Address -->
+            <div class="card mb-4" v-if="order.addresses">
+                <div class="card-header">
+                    <h3 class="card-title">Shipping Address</h3>
+                </div>
+                <div class="card-body">
+                    <address>
+                        <strong>{{ order.addresses.full_name }}</strong><br>
+                        {{ order.addresses.address }}<br>
+                        {{ order.addresses.city }}, {{ order.addresses.region?.name }}<br>
+                        {{ order.addresses.postal_code }}<br>
+                        Phone: {{ order.addresses.phone }}
+                    </address>
                 </div>
             </div>
-            </div>
-
-    <div class="order-status">
-        <span class="badge badge-primary">Order Status: {{ order.status?.name }}</span>
-        <span class="badge badge-secondary">Payment Status: {{ order.payment_status }}</span>
-        <span class="badge badge-success">Delivery Status: {{ order.delivery_status }}</span>
-    </div>
-    <div class="order-address">
-        <h2>Shipping Address</h2>
-        <p>{{ order.addresses ? order.addresses.full_name + ', ' + order.addresses.address + ', ' + order.addresses.region?.name : "" }}</p>
-        <p>Phone: {{ order.addresses ? order.addresses.phone : "" }}</p>
-        <p>Postal Code: {{ order.addresses ? order.addresses.postal_code : "" }}</p>
-        <p>City: {{ order.addresses ? order.addresses.city : "" }}</p>
-        <p>Country: {{ order.addresses ? order.addresses.country : "" }}</p>
-        <p>Created At: {{ order.addresses ? order.addresses.created_at : "" }}</p>
-        <p>Updated At: {{ order.addresses ? order.addresses.updated_at : "" }}</p>
-        <p>Creator: {{ order.addresses ? order.addresses.creator?.name : "" }}</p>
-        <p>Updated By: {{ order.addresses ? order.addresses.updater?.name : "" }}</p>        
-    </div>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in order.items" :key="item.id">
-                <td>
-                    <div class="product-info">
-                        <img :src="item.product.image" alt="Product Image" class="product-image">
-                        <div>
-                            <strong>{{ item.product.name }}</strong><br>
-                            {{ item.product.sku }}
+            <!-- Order Summary -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Order Summary</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 ">
+                            <table class="table table-borderless">
+                                <tr>
+                                    <td>Subtotal:</td>
+                                    <td class="text-end">${{ order.sub_total || '0.00' }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Shipping:</td>
+                                    <td class="text-end">${{ order.shipping_price || '0.00' }}</td>
+                                </tr>
+                                <tr v-if="order.discount > 0">
+                                    <td>Discount:</td>
+                                    <td class="text-end text-danger">- ${{ order.discount|| '0.00' }}</td>
+                                </tr>
+                                <tr class="border-top">
+                                    <th>Total:</th>
+                                    <th class="text-end">${{ order.total|| '0.00' }}</th>
+                                </tr>
+                            </table>
                         </div>
                     </div>
-                </td>
-                <td>
-                    <div class="quantity-control">
-                        <button class="btn btn-sm btn-outline-secondary">-</button>
-                        <span>{{ item.quantity }}</span>
-                        <button class="btn btn-sm btn-outline-secondary">+</button>
-                    </div>
-                </td>
-                <td>${{ item.price }}</td>
-                <td>
-                    <button class="btn btn-danger btn-sm">🗑</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    <div class="order-summary">
-        <h2>Order Summary</h2>
-        <div class="summary-details">
-            <p>Subtotal Price: <span>${{ order.sub_total }}</span></p>
-            <p>Shipping Cost (+): <span>${{ order.shipping_price }}</span></p>
-            <p>Discount (-): <span>${{ order.discount }}</span></p>
-            <p><strong>Total Payable:</strong> <span>${{ order.total }}</span></p>
+                </div>
+            </div>
         </div>
     </div>
 
-
-    <dl class="row">
-        <dt class="col-sm-3">id</dt>
-        <dd class="col-sm-9">{{ order.id }}</dd>
-
-        <dt class="col-sm-3">user</dt>
-        <dd class="col-sm-9">{{ order.user?.name ?? "" }} </dd>
-
-
-        <dt class="col-sm-3"> creator </dt>
-        <dd class="col-sm-9">{{ order.creator?.name ?? ""}}</dd>
-
-        <dt class="col-sm-3">status</dt>
-        <dd class="col-sm-9">{{ order.status?.name??""}}</dd>
-
-        <dt class="col-sm-3">payment_status</dt>
-        <dd class="col-sm-9">{{ order.payment_status}}</dd>
-        <dt class="col-sm-3">delivery_status</dt>
-        <dd class="col-sm-9">{{ order.delivery_status}}</dd>
-
-
-
-        <dt class="col-sm-3">address</dt>
-        <dd class="col-sm-9">{{ order.addresses ?   order.addresses.phone + "   "+ order.addresses.full_name + ', ' + order.addresses.address + ', ' + order.addresses.region?.name??""  : ""}}</dd>
-
-        <dt class="col-sm-3">shipping_price</dt>
-        <dd class="col-sm-9">{{ order.shipping_price }}</dd>
-        <dt class="col-sm-3">subtotal</dt>
-        <dd class="col-sm-9">{{ order.sub_total }}</dd>
-        <dt class="col-sm-3">total</dt>
-        <dd class="col-sm-9">{{ order.total }}</dd>
-        <dt class="col-sm-3">created_at</dt>
-        <dd class="col-sm-9">{{ order.created_at }}</dd>
-        <dt class="col-sm-3">updated_at</dt>
-        <dd class="col-sm-9">{{ order.updated_at }}</dd>
-
-    </dl>
-
-    <button class="btn btn-primary" @click="processOrder">process order</button>
+    <!-- Action Buttons -->
+    <div class="mt-4">
+        <button class="btn btn-primary" @click="processOrder" v-if="order.status?.name !== 'processing'">
+            Process Order
+        </button>
+        <a :href="'/orders/index'" class="btn btn-outline-secondary ms-2">
+            Back to Orders
+        </a>
+    </div>
 </div>
+
 
 <?php
 $js = <<<JS
